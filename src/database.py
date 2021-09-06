2be5f679257584ad1564d7562aea6dbdddf37911
NickMale <nmale98@gmail.com>
2021-09-05 20:56:06 -0400

    feature(database): Write fighters to database

--- src/database.py
+++ src/database.py
@@ -2,10 +2,28 @@ from sqlalchemy import create_engine
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relation, sessionmaker, relationship
+import logging
+from psycopg2.errors import UniqueViolation
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.session import Session
+from scraper import fetch_bouts, print_bouts
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 db_string = "postgresql://postgres@localhost:5432"
+logger.debug(f'Connecting to database at {db_string}')
 db = create_engine(db_string)
 base = declarative_base()
+
+def persist_fighter(session: Session, fighter_first: String, fighter_last) -> None:
+    try:
+        fighter = Fighter(fighter_first=fighter_first, fighter_last=fighter_last)
+        session.add(fighter)
+        session.commit()
+        logger.info(f'Persisted {fighter_first + " " + fighter_last} to database.')
+    except IntegrityError as e:
+        logger.warning(f'Fighter already exists in database {fighter_first + " " + fighter_last}')
+        session.rollback()
 
 class Fighter(base):
     __tablename__ = 'fighters'
@@ -39,12 +57,11 @@ class Bout(base):
 session_setup = sessionmaker(db)
 session = session_setup()
 base.metadata.create_all(db)
-fighter = Fighter(fighter_first="Alistair", fighter_last="Overeem")
-session.add(fighter)
-session.commit()
-fighter = Fighter(fighter_first="Matt", fighter_last="Brown")
-session.add(fighter)
-session.commit()
-fighters = session.query(Fighter)
-for combatant in fighters:
-    print(f'{combatant.fighter_first} {combatant.fighter_last}')
+bouts = fetch_bouts()
+for bout in bouts:
+    winner_first = bout['winner'].split(' ', 1)[0]
+    winner_last = bout['winner'].split(' ', 1)[1]
+    loser_first = bout['loser'].split(' ', 1)[0]
+    loser_last = bout['loser'].split(' ', 1)[1]
+    persist_fighter(session, winner_first, winner_last)
+    persist_fighter(session, loser_first, loser_last)
--- src/scraper.py
+++ src/scraper.py
@@ -4,22 +4,31 @@ from types import resolve_bases
 from typing import List
 import requests
 from bs4 import BeautifulSoup
+from sqlalchemy.sql.sqltypes import String
+from datetime import datetime
 
 event_url = 'http://ufcstats.com/event-details/2f13e4020cea5b38'
 base_page = 'http://ufcstats.com/statistics/events/completed?page=all'
 
 
-def extract_pages(base_url):
+def extract_pages(base_url: String):
+    # TODO: Fix this, hacky. Returns a list of tuples, the link for the page and the date the event ocurred
     page = requests.get(base_url)
     soup = BeautifulSoup(page.content, "html.parser")
     sections = soup.find_all(class_='b-statistics__table-content')
+    date_strings = soup.find_all(class_="b-statistics__date")
     links = []
     for section in sections:
         links.append(section.find("a").get('href'))
+    # Top link will be next upcoming event which will not contain results,
+    # discard it
     links = links[1:]
-    return links
+    date_strings = date_strings[1:]
+    dates_converted = map(lambda x: datetime.strptime(x.text.strip(), '%B %d, %Y').date(), date_strings)
+    zipped_list = list(zip(links, dates_converted))
+    return zipped_list
 
-def extract_bouts(soup):
+def extract_bouts(soup, date):
     results = soup.find_all(class_="b-fight-details__table-row")
     results = results[1:len(results)]
     bouts = []
@@ -33,7 +42,10 @@ def extract_bouts(soup):
             outcome = item.find_all("td")[7].text.strip()
             major_outcome = outcome.split()[0]
             minor_outcome = ' '.join(outcome.split()[1:]) if len(outcome.split()) else ""
-            bout = { 'winner': winner, 'loser': loser, 'weight_class': weight_class, 'major_outcome' : major_outcome, 'minor_outcome': minor_outcome, 'is_draw': is_draw }
+            bout = { 'winner': winner, 'loser': loser, 
+                'weight_class': weight_class, 'major_outcome' : major_outcome,
+                'minor_outcome': minor_outcome, 'is_draw': is_draw,
+                'bout_date': date }
             bouts.append(bout)
         except Exception as e:
             print(item, file = stderr)
@@ -42,17 +54,20 @@ def extract_bouts(soup):
 def print_bouts(bouts: List):
     for bout in bouts:
         if not bout['is_draw']:
-            print(f'{bout["winner"]} defeats {bout["loser"]} at {bout["weight_class"]} via {bout["major_outcome"]}')
+            print(f'{bout["winner"]} defeats {bout["loser"]} at {bout["weight_class"]} via {bout["major_outcome"]} on {bout["bout_date"]}')
         else:
-            print(f'Draw between {bout["winner"]} and {bout["loser"]} at {bout["weight_class"]} via {bout["major_outcome"]}')
+            print(f'Draw between {bout["winner"]} and {bout["loser"]} at {bout["weight_class"]} via {bout["major_outcome"]} on {bout["bout_date"]}')
 
 def fetch_bouts():
     bouts = []
-    links = extract_pages(base_page)
-    for link in links:
+    link_date_touples = extract_pages(base_page)
+    for touple in link_date_touples:
+        link = touple[0]
+        date = touple[1]
         page = requests.get(link)
         soup = BeautifulSoup(page.content, "html.parser")
-        page_bouts = extract_bouts(soup)
+        page_bouts = extract_bouts(soup, date)
         bouts.extend(page_bouts)
+    return bouts
 
 
